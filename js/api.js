fc46243b91a4401f56c7bbbe6beb5c6ff61c212a
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-08-17 14:50:25 +0800

    feat: add initial html and api functionality

---
+++ js/api.js
@@ -0,0 +1,42 @@
+const API_BASE =
+    "http://localhost:8080/tictactoe/tictactoeserver";
+
+async function createGame(key) {
+    const response = await fetch(
+        `${API_BASE}/createGame?key=${encodeURIComponent(key)}`
+    );
+
+    return response.text();
+}
+
+async function checkGame(key) {
+    const response = await fetch(
+        `${API_BASE}/check?key=${encodeURIComponent(key)}`
+    );
+
+    return response.text();
+}
+
+async function getBoard(key) {
+    const response = await fetch(
+        `${API_BASE}/board?key=${encodeURIComponent(key)}`
+    );
+
+    return response.text();
+}
+
+async function makeMove(key, tile, x, y) {
+    const response = await fetch(
+        `${API_BASE}/move?key=${encodeURIComponent(key)}&tile=${tile}&y=${y}&x=${x}`
+    );
+
+    return response.text();
+}
+
+async function resetGame(key) {
+    const response = await fetch(
+        `${API_BASE}/reset?key=${encodeURIComponent(key)}`
+    );
+
+    return response.text();
+}
